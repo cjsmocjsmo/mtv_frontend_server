2c18027bbf75fec5d4627108f4e94820a2c6e7a2
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-06-06 12:24:30 -0700

    terst

--- buildMTV.sh
+++ buildMTV.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-MTV="/media/pi/HD/MTVSERVER/mtv2.2";
-FES="/media/pi/HD/MTVSERVER/mtv_frontend_server";
+MTV="/media/pi/mtv2.2";
+FES="/media/pi/mtv_frontend_server";
 VERSION="0.0.1";
 IMG="mtv_frontend_server:$VERSION";
 
