91324a871150e2625e92aee5d01da117c776289b
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-06-06 12:18:41 -0700

    update

--- buildMTV.sh
+++ buildMTV.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-MTV=$HOME/mtv2.2;
-FES=$HOME/mtv_frontend_server;
+MTV="/media/charliepi/HD/MTVSERVER/mtv2.2";
+FES="/media/charliepi/HD/MTVSERVER/mtv_frontend_server";
 VERSION="0.0.1";
 IMG="mtv_frontend_server:$VERSION";
 
@@ -12,16 +12,16 @@ if [ -d "$MTV" ]; then
     cd $MTV;
     git pull;
     npm run build;
-    cp -pvr dist/ $FES/data/;
+    cp -pvr dist/* $FES/data/;
 else
     echo "MTV does not exist";
     git clone https://github.com/cjsmocjsmo/mtv2.2.git;
     cd $MTV;
     npm run build;
-    cp -pvr dist/ $FES/data/;
+    cp -pvr dist/* $FES/data/;
 fi
 
-cd $FES;
-git pull;
+# cd $FES;
+# git pull;
 docker build -t $IMG .;
 docker run -d -p 8456:8456 $IMG;
